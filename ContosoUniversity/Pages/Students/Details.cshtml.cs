a197b68781413e07a6a59f3572ad689285f48ace
DiegoDoria <retrexretrex@hotmail.com>
2024-05-20 16:10:24 -0500

    Paginacion y ordenamiento terminado

--- ContosoUniversity/Pages/Students/Details.cshtml.cs
+++ ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -19,7 +19,7 @@ namespace ContosoUniversity.Pages.Students
             _context = context;
         }
 
-      public Student Student { get; set; }
+        public Student Student { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -28,14 +28,11 @@ namespace ContosoUniversity.Pages.Students
                 return NotFound();
             }
 
-            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
-            if (student == null)
+            Student = await _context.Students.Include(s => s.Enrollments).ThenInclude(e => e.Course).AsNoTracking().FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
             {
                 return NotFound();
-            }
-            else 
-            {
-                Student = student;
             }
             return Page();
         }
